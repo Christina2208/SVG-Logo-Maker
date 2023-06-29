f313357c2662b010e11e059463724ba578399d33
Christina2208 <christinalf22@gmail.com>
2023-06-28 22:05:31 -0400

    text input

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const fs = require('fs');
 const inquirer = require('inquier');
 const generateSVG = require('./lib/shapes');
 
+// prompt
+const promptUser = () => {
+    inquirer
+    .prompt([
+        {
+//text input for the SVG logo text, 3 charcters maximum
+            name: 'text',
+            message: 'Enter up to three characters that you want displayed on the logo',
+            validate: (input) => {
+                if (input.length > 3) {
+                    return 'the limit is 3 characters, please re-enter your input';
+                }
+                return true;
+            },
+        },
+
+    ])
+}
